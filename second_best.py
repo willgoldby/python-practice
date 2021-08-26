459cccbd78391a7fbfdbd6f0e58052beb3b0a03a
William Goldby <willgoldby@gmail.com>
2021-08-25 17:08:14 -0700

    Add solution.

--- second_best.py
+++ second_best.py
@@ -1,5 +1,23 @@
+from collections import namedtuple
 
-def what(n, k):
-    n = input()
-    k = input()
-
+def second_best(n, name_scores):
+    Student = namedtuple('Name', 'score')
+    students = []
+    if n == len(name_scores):
+        for student in name_scores:
+            print(student)
+            name = student[0]
+            score = student[1]
+            name = Student(name, score)
+            students.append(name)
+    print(students)
+        
+if __name__ == '__main__':
+    stats = []
+    num = int(input())
+    print(num)
+    for _ in range(num):
+        name = input()
+        score = float(input())
+        stats.append([name, score])
+    second_best(num, stats)
