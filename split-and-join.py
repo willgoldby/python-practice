10365f38dc75f3177d555c5572b1e114335aae82
William Goldby <willgoldby@gmail.com>
2021-09-08 15:47:20 -0700

    Add split string exercise.

---
+++ split-and-join.py
@@ -0,0 +1,10 @@
+# Split string on space and join with a dash. 
+def split_and_join(line):
+    a = line.split(" ")
+    a = "-".join(a)
+    return a
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
